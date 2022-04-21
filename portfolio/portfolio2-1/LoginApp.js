25a39c50509f2be84ae4a4e6f017d74ece1ad8d4
hannabui <97701564+hannabui@users.noreply.github.com>
2022-04-20 22:26:29 -0400

    working

--- portfolio/portfolio2-1/LoginApp.js
+++ portfolio/portfolio2-1/LoginApp.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { useState, useCallback } from "react";
-import { StyleSheet, Text, Button, View, TextInput } from "react-native";
+import { StyleSheet, Text, Button, View, TextInput, Image } from "react-native";
 
 export default function LoginApp() {
     let [username, setUsername] = useState("");
     let [password, setPassword] = useState("");
     let [loggedIn, setLoggedIn] = useState(false);
     let [errorMsg, setErrorMsg] = useState(false);
+    let [chocolate, setChocolate] = useState(false);
+    let [ice, setIce] = useState(false);
+    let [rich, setRich] = useState(false);
+    let [result, setResult] = useState('');
+    let [milkTea, setMilkTea] = useState(false);
     let welcome = "Welcome, please enter a username below!  \n  The secret password is 'MilkTea.'"
     let error = "Incorrect password. Please try again."
 
@@ -18,20 +23,43 @@ export default function LoginApp() {
             setErrorMsg(true);
         }
     }, [username, password])
+
+    let submitForm=()=>{
+        if((chocolate && ( ice || rich)) || (ice && (chocolate || rich)) || (rich && ( ice || chocolate)) ){
+            setResult("You are a Milk Tea!")
+            setMilkTea(true)
+        }
+        else{
+            setResult("You are a Fruit Tea!")
+            setMilkTea(false)
+        }
+    }
+
     return (
         loggedIn ? (
             <View style={styles.container}>
                 <Text>Welcome {username}!</Text>
 
                 <Text>Do you like chocolate or candy more?</Text>
-                <Button title="chocolate"></Button>
-                <Button title="candy"></Button>
+                <Button title="chocolate" onPress={()=> setChocolate(true)} ></Button>
+                <Button title="candy" onPress={()=> setChocolate(false)}></Button>
+
                 <Text>Would you have fruits or icecream for dessert?</Text>
-                <Button title="fruits"></Button>
-                <Button title="icecream"></Button>
+                <Button title="icecream" onPress={()=> setIce(true)}></Button>
+                <Button title="fruits" onPress={()=> setIce(false)}></Button>
+
                 <Text>Do you like something sweet that's rich or refreshing?</Text>
-                <Button title="rich"></Button>
-                <Button title="refreshing"></Button>
+                <Button title="rich" onPress={()=> setRich(true)}></Button>
+                <Button title="refreshing" onPress={()=> setRich(false)}></Button>
+
+                <Button title="submit" onPress={()=> submitForm()}></Button>
+
+                <Text>{result}</Text>
+               {
+                   result!='' &&
+                    <Image source={require(milkTea?"../portfolio2-1/assets/milktea.jpg":"../portfolio2-1/assets/fruittea.jpg" )} style={{width:280, height:180}} />
+                   
+               }
 
             </View>
             ):(
